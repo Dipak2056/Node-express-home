463d7542a6db38f8a6aaebbed99a7a4bafe463c7
Dipak 2056 <paudelsantosh508@gmail.com>
2022-04-18 07:14:04 +1000

    Event emitter modules

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
 const EventEmitter = require("events");
 const emitter = new EventEmitter();
 //Register a listener
-emitter.on("messageLogged", function () {
-  console.log("listener Called");
+emitter.on("messageLogged", function (arg) {
+  console.log("listener Called", arg);
 });
-//Raise an event
-emitter.emit("messageLogged");
 
-//we cannot register a listener after raising the event
-//because what happens, raising the event function go and iterate all the
-//linesof code which are registerring before it
+const log = require("./logger");
+log("message");
--- logger.js
+++ logger.js
@@ -1,7 +1,13 @@
+const EventEmitter = require("events");
+const emitter = new EventEmitter();
+
 var url = "http://dipakpaudel.io";
 
 function log(message) {
   console.log(message);
 }
+
+//Raise an event
+emitter.emit("messageLogged", { id: 1, url: "https://" });
 
 module.exports.log = log;
