4103f44f3e65fb5e2550fdcb63e73d5db4285dcf
Dipak 2056 <paudelsantosh508@gmail.com>
2022-04-18 06:57:05 +1000

    event module

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
-const fs = require("fs");
-
-const files = fs.readdir("./", function (error, files) {
-  if (error) console.log("Error", error);
-  else console.log("Result: ", files);
+const EventEmitter = require("events");
+const emitter = new EventEmitter();
+//Register a listener
+emitter.on("messageLogged", function () {
+  console.log("listener Called");
 });
+//Raise an event
+emitter.emit("messageLogged");
+
+//we cannot register a listener after raising the event
+//because what happens, raising the event function go and iterate all the
+//linesof code which are registerring before it
